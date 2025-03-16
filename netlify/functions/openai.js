7557c59d74877fde7683f883b4b1be6b07d06509
AlexRNetworks <ctpardue@icloud.com>
2025-03-16 14:09:51 -0400

    Update openai.js

--- netlify/functions/openai.js
+++ netlify/functions/openai.js
@@ -9,13 +9,13 @@ exports.handler = async function (event, context) {
         const body = JSON.parse(event.body);
         const { prompt } = body;
 
-        let stage1Prompt = `Revise the following text to sound like a student explaining a concept. Use simple vocabulary, short sentences, and repetitive phrases. Include minor grammatical errors and awkward phrasing. Do not add or remove content. Make the text sound unpolished and imperfect. Here are some examples of the desired style:
+        let stage1Prompt = `Revise the following text to sound like a student explaining a concept. Use simple vocabulary short sentences and repetitive phrases. Include consistent grammatical errors and awkward phrasing. Do not use commas or question marks. Make the text sound unpolished and imperfect with abrupt shifts in thought. Here are some examples of the desired style:
 
-        Example 1: "Recycling is a good thing for our world. The throwing away of bottles, cans and paper is the enormous to make our land dirty and bad. To begin with, bottles is found in almost every area of our homes. From the start of when we drink from a bottle to when we throw it away we are adding to trash."
+        Example 1: "Recycling is a good thing for our world. The throwing away of bottles cans and paper is the enormous to make our land dirty and bad. To begin with bottles is found in almost every area of our homes. From the start of when we drink from a bottle to when we throw it away we are adding to trash."
 
-        Example 2: "Fast food is a big problem for peoples health. The eating of hamburgers, fries and soda is the enormous to make kids fat and sick. To begin with, hamburgers is found in almost every area of our eating."
+        Example 2: "Fast food is a big problem for peoples health. The eating of hamburgers fries and soda is the enormous to make kids fat and sick. To begin with hamburgers is found in almost every area of our eating."
 
-        Now, revise the following text:
+        Now revise the following text:
 
         ${prompt}`;
 
